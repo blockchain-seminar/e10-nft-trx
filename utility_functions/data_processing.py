bc4b4ee9c90ea64fd63d1b2a70c5cd18aaa0e037
gurtntan <gurtntan@gmail.com>
2024-06-07 02:06:14 +0200

    Update data_processing.py

--- utility_functions/data_processing.py
+++ utility_functions/data_processing.py
@@ -1,10 +1,39 @@
-from config import web3
+import os
+import pandas as pd
+from config import w3
 from utility_functions import get_contract_abi
 from utility_functions.abi_processing import get_known_token_info
 from utils import setup_logger
 import json
 
 logger = setup_logger()
+
+
+def parse_abi_events(contract_address):
+    ''' Used for parsing the fetched ABIs, results dumped in DB'''
+    # Load the ABI from a JSON file
+    with open(f'abi/{contract_address}.json', 'r') as file:
+        abi_json = json.load(file)
+
+    abi_events = [abi for abi in abi_json if abi["type"] == "event"]
+    df = pd.DataFrame()
+    id=0
+    for event in abi_events:
+        event['contract_address'] = contract_address
+        event['id'] = id
+        df_event = pd.DataFrame(event)
+        try:
+            for input in event['inputs']:
+                input['id'] = id
+                input = pd.DataFrame([input])
+                input.columns = ['input_' + col for col in input.columns]
+                merged_df = pd.merge(df_event, input, left_on='id', right_on='input_id', how='inner')
+            merged_df.drop(columns=['inputs'], inplace=True)         
+            df = pd.concat([df,merged_df])
+        except:
+            pass
+        id += 1
+    return merged_df
 
 
 def determine_contract_type(log):
@@ -15,7 +44,7 @@ def determine_contract_type(log):
     """
     erc721_interface_id = "0x80ac58cd"
     erc1155_interface_id = "0xd9b67a26"
-    token_contract = web3.eth.contract(address=log['address'], abi=[{
+    token_contract = w3.eth.contract(address=log['address'], abi=[{
         "constant": True,
         "inputs": [{"name": "interfaceID", "type": "bytes4"}],
         "name": "supportsInterface",
@@ -57,10 +86,10 @@ def get_traded_price_and_currency(transaction, log):
     # Check if transaction is in eth
     value_eth = transaction.value
     if value_eth > 0:
-        traded_price = web3.from_wei(value_eth, 'ether')
+        traded_price = w3.from_wei(value_eth, 'ether')
 
     # If log represents an ERC-20 token transfer, get the token details
-    if log['topics'][0].hex() == web3.keccak(text="Transfer(address,address,uint256)").hex():
+    if log['topics'][0].hex() == w3.keccak(text="Transfer(address,address,uint256)").hex():
         token_contract = log['address']
         token_value = parse_int_from_data(log['data']) if log['data'] else "Data Not Available"
 
@@ -73,7 +102,7 @@ def get_traded_price_and_currency(transaction, log):
         else:
             try:
                 erc20_abi = get_contract_abi(token_contract)
-                token = web3.eth.contract(address=token_contract, abi=erc20_abi)
+                token = w3.eth.contract(address=token_contract, abi=erc20_abi)
                 erc20_abi_json = json.loads(erc20_abi)
 
                 # Check if the contract has symbol and decimals functions
