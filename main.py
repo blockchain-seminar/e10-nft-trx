f0c394bc250c858cebbbac1e28a7b9ac010c7e3e
gurtntan <gurtntan@gmail.com>
2024-06-20 09:22:10 +0200

    args param

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
+import argparse
 from data_processing import enrich, fetch_and_process_receipts, fetch_and_store_transactions_in_block_range
 from db_connections import get_latest_blocks, read_from_db
 from utils import setup_logger
 
-def initialize():
-    # TODO
-    pass
-
-def main():
+def main(mode,n):
     logger = setup_logger()
-    n = 10 # for mode 3
-    mode = 3 # 1 = fetch and store new blocks and transactions for specified n; 2 = fetch and store the receipts and logs; 3 = final processing of data 
 
     if mode == 1:
         block_latest, block_lowest_fetched, block_highest_fetched = get_latest_blocks()
@@ -28,5 +23,9 @@ def main():
         enrich()  
 
 if __name__ == "__main__":
-    initialize()
-    main()
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('--mode', type=int, choices=[1, 2, 3], default=1, help='Mode: 1 = fetch and store new blocks and transactions for specified n; 2 = fetch and store the receipts and logs; 3 = final processing of data')
+    parser.add_argument('--n', type=int, default=10, help='Number of blocks to fetch, required when mode is 1')
+
+    args = parser.parse_args()
+    main(args.mode, args.n)
